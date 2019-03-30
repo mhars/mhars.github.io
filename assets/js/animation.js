97fea606ae7f3bb712e63899ce5c34d172963f1a
Michelle Hardi <michelle.hardi@rea-group.com>
2019-03-30 23:45:38 +1100

    updated content

---
+++ assets/js/animation.js
@@ -0,0 +1,7 @@
+var animation = lottie.loadAnimation({
+  container: document.getElementById('anim'),  // the dom element that will contain the animation
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'assets/lottie-anim/createaccount_data.json' // the path to the animation json
+});
